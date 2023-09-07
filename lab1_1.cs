0502f8c805fe8a0cc4679f26bd98c6cafd1bc9dd
IMREYZ <99436876+IMREYZ@users.noreply.github.com>
2023-09-07 22:44:37 +0300

    Create lab1_1.cs

---
+++ lab1_1.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double perimetr = double.Parse(Console.ReadLine());
+            double side = perimetr / 3;
+            double square = Math.Sqrt(perimetr * (perimetr - side) * (perimetr - side) * (perimetr - side));
+
+            Console.WriteLine("Сторона Площадь");
+            Console.WriteLine($" {string.Format("{0:N2}", side)}   {string.Format("{0:N2}", square)}");
+ 
+        }
+    }
+}
